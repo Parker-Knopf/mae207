8a19377819fe2452bf79e86277929384ff602443
Parker Knopf <parker.knopf@gmail.com>
2023-11-29 18:56:35 -0800

    Serial fixes

--- haptoPath/serialM.h
+++ haptoPath/serialM.h
@@ -2,7 +2,7 @@ class SerialM {
 
   // Serial Protocol
   double baudeRate = 115600;
-  const String c = " | "; // Serial Data seperator
+  const String c = "|"; // Serial Data seperator
   const String d = "D:"; // Data Indicator
   const String z = "Z:"; // Zero Indicator
   const byte indSize = 2; // Size of serial Protocol indicator
@@ -24,12 +24,11 @@ class SerialM {
 
     bool readData() {
       if (Serial.available() > 0) { //send data only when you recieve data
-          rawData = Serial.read();
+          rawData = Serial.readString();
 
         if (rawData.indexOf(d) == 0) {
           findIndex();
           parseData();
-          Serial.println(D[1]); // Only for testing purposes
           return true;
         }
         else if (rawData.indexOf(z) == 0) {
@@ -52,18 +51,18 @@ class SerialM {
   private:
 
       void findIndex() {
-          dataIndex[0] = indSize;
-          dataIndex[dataSize] = sizeof(rawData) + 1;
-          int index[dataSize - 1] = {rawData.indexOf(c)};
+          dataIndex[0] = indSize+1;
 
-          for (byte i = 1; i < dataSize -1; i++) {
-            dataIndex[i] = index[i-1];
+          for (byte i = 1; i < dataSize; i++) {
+            dataIndex[i] = rawData.indexOf(c, dataIndex[i-1])+1;
           }
+
+          dataIndex[dataSize] = rawData.length()+1;
         }//end of findIndex
 
       void parseData() {
         for (byte i = 0; i < dataSize; i++){
-          data[i] = rawData.substring(dataIndex[i], dataIndex[i+1]);
+          data[i] = rawData.substring(dataIndex[i], dataIndex[i+1]-1);
           D[i] = data[i].toFloat();
         }
       }//end of parseData
