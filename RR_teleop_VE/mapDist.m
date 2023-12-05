26d93275f7a58fb369ee2253b0c938c068ec6c49
YFeteih <yfeteih@ucsd.edu>
2023-12-05 11:20:32 -0800

    updated step

--- RR_teleop_VE/mapDist.m
+++ RR_teleop_VE/mapDist.m
@@ -1,13 +1,16 @@
 function [dMap] = mapDist(d, dMax, t)
 %UNTITLED Summary of this function goes here
 %   this function will map the raw distance values fr
-    hMax = 25; %[mm] maximum height displacement should be Rc = 25 mm
+    hMax = 23; %[mm] maximum height displacement should be Rc = 25 mm
     % function
-    tSamp = 4;
-    if rem(t,tSamp) ~= 0
-        dMap = hMax - (hMax/dMax)*d;
+    tSamp = 10; % duty cycle
+    dTemp = hMax - (hMax/dMax)*d;
+    remainder = rem(t,tSamp);
+    pulse = 5; % pulse/tSamp is the pulse
+    if remainder < pulse
+        dMap = dTemp;
     else
-        dMap = 0;
+        dMap = 0.25*dTemp;
     end
     
     if d == 0 % whatever being passed in outside threshold
